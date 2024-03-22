774c6a29840b2aeaf4cbf49f370b23108070c0a0
Samela1209 <Smkondweni7@gmail.com>
2024-03-22 11:06:24 +0200

    Injected the HTML from JavaScript

--- src/index.js
+++ src/index.js
@@ -58,7 +58,31 @@ function handleSearchSubmit(event) {
  
   searchCity(searchInput.value)
 }
+
+function displayForecast(){
+  let days = ["Tue", "Wed", "Thur", "Fri", "Sat"];
+  let forecastHtml = "";
+
+  days.forEach(function (day){
+    forecastHtml =
+      forecastHtml  +
+      ` 
+   <div class="weather-forecast-day">
+        <div class="weather-forecast-date">${day}</div>
+        <img src="https://shecodes-assets.s3.amazonaws.com/api/weather/icons/few-clouds-day.png"
+            class="weather-forecast-icon" />
+        <div class="weather-forecast-temperature">
+            <span class="weather-forecast-temperature-max"><strong>25°</strong> </span>
+            <span class="weather-forecast-temperature-min">18°</span>
+        </div>
+        `;
+  })
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);   
 
 searchCity("East London");
+
+displayForecast()
